15047e4dac4472cf88286957628ee23bc9622f09
richard8301 <richard8301@hotmail.com>
2019-06-25 04:26:06 +0800

    page layout

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
-import Footer from '../components/footer'
-import Header from "../components/header"
+import Layout from '../components/layout'
 
 const BlogPage = () => {
     return (
