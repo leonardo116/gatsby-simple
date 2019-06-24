dda90a536c16bb83c9acc7cd4fdb0f2017233199
richard8301 <richard8301@hotmail.com>
2019-06-25 04:01:54 +0800

    add the gatsby simple page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Gatsby</h1>
+            <p>I am a full-stack developer.</p>
+        </div>
+    )
+}
+
+export default AboutPage
